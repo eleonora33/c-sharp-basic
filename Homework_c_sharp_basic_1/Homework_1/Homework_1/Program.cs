5f9d7817c7fd0de0a8c1badc4cf07036f4c5e2e8
Elleonora <eleonora_todorovska@yahoo.com>
2023-03-04 01:34:58 +0100

    Add c# basic  homework 1

---
+++ Homework_c_sharp_basic_1/Homework_1/Homework_1/Program.cs
@@ -0,0 +1,54 @@
+﻿
+// Task 1
+using System.Data.Common;
+
+Console.WriteLine("Enter first number:");
+var numberOne = Console.ReadLine();
+bool numOne = int.TryParse(numberOne, out int parsedNumber1);
+
+Console.WriteLine("Enter second number:");
+var numberTwo = Console.ReadLine();
+bool numTwo = int.TryParse(numberTwo, out int parsedNumber2);
+
+Console.WriteLine("Enter operator:");
+var operation = Console.ReadLine();
+
+var result = 0;
+
+if(operation == "+")
+{
+    result = parsedNumber1 + parsedNumber2;
+}
+else if(operation == "-")
+{
+    result = parsedNumber1 - parsedNumber2;
+}
+else if (operation == "*")
+{
+    result = parsedNumber1 * parsedNumber2;
+}
+else if(operation == "/")
+{
+    result = parsedNumber1 / parsedNumber2;
+}
+else 
+{
+    Console.WriteLine("Enter valid operator");
+}
+
+Console.WriteLine("Calculate...{0}", result);
+Console.WriteLine("Calculate...{0}fjksdjfk {1} sdsadshkj {2}", result, "Zdravo", 55);
+Console.WriteLine($"Calculate...{result}");
+Console.WriteLine(@$"Calculate...
+{result}
+I'm in new line/");
+
+
+switch (operation)
+{
+    case "+":
+    default:
+        break;
+}
+
+
